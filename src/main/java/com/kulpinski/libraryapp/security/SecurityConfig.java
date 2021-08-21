697da8ce9ef93a01bb9899406629e0ab2aaeb640
pkulpinski <malpaonev@gmail.com>
2021-08-21 20:09:39 +0200

    Added permissions for user and admin

--- src/main/java/com/kulpinski/libraryapp/security/SecurityConfig.java
+++ src/main/java/com/kulpinski/libraryapp/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kulpinski.libraryapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,11 +36,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.headers().disable();
         http.authorizeRequests()
-                .antMatchers("/api/books/all").authenticated()
+                .antMatchers(HttpMethod.GET,"/api/books/all").authenticated()
+                .antMatchers(HttpMethod.POST,"/api/books/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/api/books/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/api/books/").hasRole("ADMIN")
                 .antMatchers("/api/users/all").hasRole("ADMIN")
                 .antMatchers("/console").hasRole("ADMIN")
                 .and()
-                .formLogin();
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
     }
 
 }
